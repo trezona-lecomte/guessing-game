fd6ec55b3fd845366aad88a4b4cd87a67d56310e
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-06-16 13:47:38 +1200

    Add guess getting & say to high

--- lib/console_ui.rb
+++ lib/console_ui.rb
@@ -1,11 +1,20 @@
 class ConsoleUI
-	
-	def welcome
-		puts "Welcome to the magical number guessing game!"
-   end
 
-   def unicorn
-   	puts " *
+		def welcome
+			puts "Welcome to the magical number guessing game!"
+		end
+
+		def get_guess(lower_bound, upper_bound)
+			puts "Please enter a number between #{lower_bound} and #{upper_bound}"
+   		Integer(STDIN.gets.chomp) rescue nil
+		end
+
+		def say_too_high(guess)
+			puts "Sorry, your guess of #{guess} was too high! Try guessing lower."
+		end
+
+   	def unicorn
+   		puts " *
   *
    *     **
     *   ******
@@ -25,5 +34,5 @@ class ConsoleUI
        **                                     ****
       **                                       ***
      ***                                     ****"
-   end
+		end
 end
--- spec/console_ui_spec.rb
+++ spec/console_ui_spec.rb
@@ -2,11 +2,35 @@ require 'spec_helper'
 require 'console_ui'
 
 RSpec.describe ConsoleUI do
+	let(:lower_bound) { 1 }
+	let(:upper_bound) { 100 }
 	let(:ui) { ConsoleUI.new }
-	
+
   describe '#welcome' do
     it 'welcomes the user' do
       expect{ui.welcome}.to output("Welcome to the magical number guessing game!\n").to_stdout
     end
+  end
+
+  describe '#get_guess' do
+    context 'when given a valid guess' do
+      it 'returns guess as a number from STDIN' do
+      	allow(STDIN).to receive(:gets) { "#{upper_bound}" }
+        expect(ui.get_guess(lower_bound, upper_bound)).to eq(upper_bound)
+      end
+    end
+
+    context 'when given an invalid guess' do
+    	it 'returns nil' do
+      	allow(STDIN).to receive(:gets) { 'Womack! Why am I not surprised, you piece of shit!' }
+        expect(ui.get_guess(lower_bound, upper_bound)).to be nil
+    	end
+    end
+  end
+
+  describe '#say_too_high' do
+  	it "tells the user they've guessed too high" do
+			expect{ui.say_too_high(lower_bound)}.to output("Sorry, your guess of #{lower_bound} was too high! Try guessing lower.\n").to_stdout
+  	end
   end
 end
