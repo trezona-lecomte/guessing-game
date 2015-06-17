6ca7acddf209c8bd7ff5326b306b7aaa1c39f123
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-06-17 15:48:15 +1200

    Fix indendation & remove front-end cast to Integer

--- lib/console_ui.rb
+++ lib/console_ui.rb
@@ -1,28 +1,30 @@
 class ConsoleUI
 
-		def display_welcome_message
-			puts "Welcome to the magical number guessing game!"
-		end
+    def display_welcome_message
+      puts "Welcome to the magical number guessing game!"
+    end
 
-		def get_guess(lower_bound, upper_bound)
-			puts "Please enter a number between #{lower_bound} and #{upper_bound}"
-   		Integer(STDIN.gets.chomp) rescue nil
-		end
+    # TODO move cast/rescue parser/validator
+    def get_guess(lower_bound, upper_bound)
+      puts "Please enter a number between #{lower_bound} and #{upper_bound}"
+      STDIN.gets.chomp
+    end
 
+    # TODO handle no result
     def display_guess_result(result)
       puts "\nYour guess was #{result}!"
     end
 
-		def display_game_won_message
-			puts "\nCongratulations, you guessed correctly!"
-		end
+    def display_game_won_message
+      puts "\nCongratulations, you guessed correctly!"
+    end
 
-		def display_game_lost_message(magic_number)
-			puts "\nOh no! You've run out of chances, so you lose. The number was #{magic_number}."
-		end
+    def display_game_lost_message(magic_number)
+      puts "\nOh no! You've run out of chances, so you lose. The number was #{magic_number}."
+    end
 
-   	def display_unicorn
-   		puts " *
+    def display_unicorn
+      puts " *
   *
    *     **
     *   ******
@@ -42,5 +44,5 @@ class ConsoleUI
        **                                     ****
       **                                       ***
      ***                                     ****"
-		end
+    end
 end
--- spec/console_ui_spec.rb
+++ spec/console_ui_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 require 'console_ui'
 
 RSpec.describe ConsoleUI do
-	let(:lower_bound) { 1 }
-	let(:upper_bound) { 100 }
-	let(:ui) { ConsoleUI.new }
+  let(:lower_bound) { 1 }
+  let(:upper_bound) { 100 }
+  let(:ui) { ConsoleUI.new }
 
   describe '#display_welcome_message' do
     it 'welcomes the user' do
@@ -14,17 +14,10 @@ RSpec.describe ConsoleUI do
 
   describe '#get_guess' do
     context 'when given a valid guess' do
-      it 'returns guess as a number from STDIN' do
-      	allow(STDIN).to receive(:gets) { "#{upper_bound}" }
-        expect(ui.get_guess(lower_bound, upper_bound)).to eq(upper_bound)
+      it 'returns guess as a string from STDIN' do
+        allow(STDIN).to receive(:gets) { "9999" }
+        expect(ui.get_guess(lower_bound, upper_bound)).to eq("9999")
       end
-    end
-
-    context 'when given an invalid guess' do
-    	it 'returns nil' do
-      	allow(STDIN).to receive(:gets) { 'Womack! Why am I not surprised, you piece of shit!' }
-        expect(ui.get_guess(lower_bound, upper_bound)).to be nil
-    	end
     end
   end
 
@@ -49,9 +42,9 @@ RSpec.describe ConsoleUI do
   end
 
   describe '#display_game_lost_message' do
-  	let(:magic_number) { 99 }
-  	it "tells the user they've lost the game" do
-  		expect{ui.display_game_lost_message(magic_number)}.to output("\nOh no! You've run out of chances, so you lose. The number was #{magic_number}.\n").to_stdout
-  	end
+    let(:magic_number) { 99 }
+    it "tells the user they've lost the game" do
+      expect{ui.display_game_lost_message(magic_number)}.to output("\nOh no! You've run out of chances, so you lose. The number was #{magic_number}.\n").to_stdout
+    end
   end
 end
