6bc7457c593801283d10e5009e0189863317830b
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-06-22 08:42:47 +1200

    Add BuildGuess

---
+++ lib/build_guess.rb
@@ -0,0 +1,20 @@
+require_relative './guess.rb'
+
+class BuildGuess
+
+  def self.call(range:, guess_str:)
+    begin
+      case range.first.class.to_s
+      when 'Fixnum', 'Bignum'
+        guess = Guess.new(Integer(guess_str))
+      when 'String'
+        guess = Guess.new(guess_str)
+      end
+
+      guess = nil unless range.include?(guess.value)
+    rescue
+      guess = nil
+    end
+    guess
+  end
+end
---
+++ spec/build_guess_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require 'build_guess'
+require 'guess'
+
+RSpec.describe BuildGuess do
+
+  describe '#call' do
+    context' when passed a valid guess_str' do
+      it 'returns a guess' do
+        expect(BuildGuess.call(range: (1..100), guess_str: "1").value).to eq(Guess.new(1).value)
+        expect(BuildGuess.call(range: (1..100), guess_str: " 100 ").value).to eq(Guess.new(100).value)
+      end
+    end
+
+    context' when passed an out-of-range guess_str' do
+      it 'returns nil' do
+        expect(BuildGuess.call(range: (1..100), guess_str: "-1")).to eq(nil)
+        expect(BuildGuess.call(range: (1..100), guess_str: "0")).to eq(nil)
+        expect(BuildGuess.call(range: (1..100), guess_str: "101")).to eq(nil)
+      end
+    end
+
+    context' when passed an invalid guess_str' do
+      it 'returns nil' do
+        expect(BuildGuess.call(range: (1..100), guess_str: "klsadjf;")).to eq(nil)
+      end
+    end
+  end
+end
