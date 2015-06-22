a1ae897d26d48bbc1d5368c71afcb3b615a73e06
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-06-23 10:57:44 +1200

    Allow choice of game type - crudely

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,7 +1,23 @@
 require_relative './game_controller.rb'
-require_relative './console_ui'
+require_relative './console_ui.rb'
 require_relative './guessing_game.rb'
 
-game = GuessingGame.new(range: (1..100), chances: 6)
+range = nil
+
+until range
+  puts 'Please enter what you would like to play with:
+          a) numbers
+          b) letters'
+
+  choice = gets.chomp
+
+  if choice == 'numbers' || choice == 'a'
+    range = (1..100)
+  elsif choice == 'letters' || choice == 'b'
+    range = ('a'..'z')
+  end
+end
+
+game = GuessingGame.new(range: range, chances: 6)
 game_controller = GameController.new(game: game, ui: ConsoleUI.new)
 game_controller.play_game
