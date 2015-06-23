e5db4df091bc19aec00e779736194d1558be6cde
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-06-24 09:14:49 +1200

    Rename runner to high-low & use numbers only

---
+++ lib/high-low.rb
@@ -0,0 +1,7 @@
+require_relative './game_controller.rb'
+require_relative './console_ui.rb'
+require_relative './guessing_game.rb'
+
+game = GuessingGame.new(range: (1..100), chances: 6)
+game_controller = GameController.new(game: game, ui: ConsoleUI.new)
+game_controller.play_game
